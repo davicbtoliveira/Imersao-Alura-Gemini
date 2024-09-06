862f8170068256dde3b650b306b3c6f1fab0ca8e
Davi Oliveira <davicbtoliveira@hotmail.com>
2024-09-06 08:28:55 -0400

    Ultima aula da imersao

--- IMERSAO/app.js
+++ IMERSAO/app.js
@@ -2,12 +2,29 @@
 function pesquisar() {
     // Obtém a seção HTML onde os resultados serão inseridos
     let section = document.getElementById('resultados-pesquisa');
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+// Limpa a string de pesquisa e verifica se está vazia ou contém apenas espaços em branco
+campoPesquisa = campoPesquisa.trim().toLowerCase();
+if (campoPesquisa === "") {
+  section.innerHTML = `<p class="sem-resultados">Por favor, insira um termo de pesquisa válido.</p>`;
+  return;
+}
   
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
   
     // Itera sobre cada dado na lista de resultados
-    for (let dado of dados) {
+      for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase()
+      //  Pesquisa um titulo ou descricao que condiz com o informado na pesquisa
+        if (dado.titulo.includes(campoPesquisa) || (dado.descricao).includes(campoPesquisa) || (dado.tags).includes(campoPesquisa)) {
+
       // Cria uma nova div para cada resultado, formatando-a como HTML
       resultados += `<div class="item-resultado">
         <h2>
@@ -17,8 +34,13 @@ function pesquisar() {
           <a href="${dado.link}" target="_blank">Mais Informaçoes</a>
         </p>
       </div>`;
+      }
     }
-  
-    // Insere o HTML gerado na seção de resultados
+    
+    if (!resultados) {
+      resultados = "<p>Nada foi encontrado</p>";
+    }
+
+    //  Exibe o resultado
     section.innerHTML = resultados;
-  }
+}
--- IMERSAO/dados.js
+++ IMERSAO/dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Elden Ring",
         descricao: "Elden Ring é um RPG de ação desenvolvido pela FromSoftware, conhecida por criar experiências de jogo desafiadoras e imersivas. Ambientado em um mundo de fantasia sombria e expansivo, o jogo oferece aos jogadores uma aventura épica repleta de perigos e descobertas.",
-        link: "https://en.bandainamcoent.eu/elden-ring/elden-ring"
+        link: "https://en.bandainamcoent.eu/elden-ring/elden-ring",
+        tags: "rpg goty lancamento"
     },
     {
         titulo: "Red Dead Redemption 2",
         descricao: "Red Dead Redemption 2 é um jogo de mundo aberto ambientado no Velho Oeste. Siga a história de Arthur Morgan e da gangue de Van der Linde em uma jornada épica.",
-        link: "https://www.rockstargames.com/br/reddeadredemption2"
+        link: "https://www.rockstargames.com/br/reddeadredemption2",
+        tags: "velho oeste gta gang"
     },
     {
         titulo: "It Takes Two",
         descricao: "It Takes Two é um jogo cooperativo que celebra o amor e a amizade, com desafios divertidos e uma história emocionante.",
-        link: "https://www.ea.com/pt-br/games/it-takes-two"
+        link: "https://www.ea.com/pt-br/games/it-takes-two",
+        tags: "coop 2 "
     }
 ];
